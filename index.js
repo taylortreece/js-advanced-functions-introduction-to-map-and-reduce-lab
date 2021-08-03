273b6a0707f9428fd5b4988f57daef00470054a2
taylortreece <tayloratreece@gmail.com>
2021-08-02 22:10:16 -0500

    commit tree e3903226e7a3b76d2db4a7d6cbc503fd5d215092

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+function mapToNegativize(array) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        debugger
+        newArray.push(-array[i])
+    }
+    return newArray
+}
+
+function mapToNoChange(array) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+function mapToDouble(array) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(array) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * array[i]);
+    }
+    return newArray
+}
+
+function reduceToTotal(array, start=0) {
+    let total = start;
+    for(const element of array) {
+        total += element
+    }
+    return total
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (!array[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for(const element of array) { 
+        if(!!element) return true;
+    }
+    return false
+}
